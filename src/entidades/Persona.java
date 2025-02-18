a26eb7e8b5cb32a7135fd8bf1fbda92ce455fe77
stivenzxy <jesuspereztorres420@gmail.com>
2025-02-18 08:53:56 -0500

    first commit - Person Class

---
+++ src/entidades/Persona.java
@@ -0,0 +1,48 @@
+package entidades;
+
+
+public abstract class Persona {
+    private double ID;
+    private String nombres;
+    private String apellidos;
+    private String email;
+
+    Persona(double ID, String nombres, String apellidos, String email) {
+        this.ID = ID;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.email = email;
+    }
+
+    public double getID() {
+        return ID;
+    }
+
+    public void setID(double ID) {
+        this.ID = ID;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
